2de909e9b36722469487c4042f4b63eb36bb7614
AniketDubey <39259030+AniketDubey@users.noreply.github.com>
2019-10-11 21:27:38 +0530

    1

---
+++ 1.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	int sum=0;
+	scanf("%d",&a);
+	int b[a];
+	for(int i=0;i<a;i++)
+	{
+		scanf("%d",&b[i]);
+	}
+	for(int j=0;j<a;j++)
+	{
+		sum=sum+b[j];
+	}
+	printf("%d",sum);
+    return 0;
+}
