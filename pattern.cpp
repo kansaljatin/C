92bb27c60b67655c431aaa8c5b8d7d7b13d9a053
jatin kansal <56547404+kansaljatin@users.noreply.github.com>
2019-10-25 10:58:49 -0700

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -7,8 +7,10 @@ int main()
     scanf("%d", &n);
     
     for(int i = n; i >= 1; i--){
-        for(int j = n; j > i; j--)
+        int j=n;
+        while(j > i)
             printf("%d ", j);
+            j--;
         for(int j = 1; j <= 2 * i - 1; j++)
             printf("%d ", i);
         for(int j = i + 1; j <= n; j++)
@@ -17,8 +19,10 @@ int main()
     }
     
     for(int i = 1; i < n; i++){
-        for(int j = n; j > i; j--)
+        int j=n;
+        while(j > i)
             printf("%d ", j);
+            j--;
         for(int j = 1; j <= 2 * i - 1; j++)
             printf("%d ", i + 1);
         for(int j = i + 1; j <= n; j++)
