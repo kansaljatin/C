4e452a1a0676ddad2198e365478dceba487fb2df
jatin kansal <56547404+kansaljatin@users.noreply.github.com>
2019-10-25 11:25:45 -0700

    Update digit frequency.cpp

--- digit frequency.cpp
+++ digit frequency.cpp
@@ -9,25 +9,25 @@ int main()
 	for(i=0;i<strlen(c);i++)
 	{
 		if(c[i]=='0')
-		s0=s0+1;
+		s0++;
 		if(c[i]=='1')
-		s1=s1+1;
+		s1++;
 		if(c[i]=='2')
-		s2=s2+1;
+		s2++;
 		if(c[i]=='3')
-		s3=s3+1;
+		s3++;
 		if(c[i]=='4')
-		s4=s4+1;
+		s4++;
 		if(c[i]=='5')
-		s5=s5+1;
+		s5++;
 		if(c[i]=='6')
-		s6=s6+1;
+		s6++;
 		if(c[i]=='7')
-		s7=s7+1;
+		s7++;
 		if(c[i]=='8')
-		s8=s8+1;
+		s8++;
 		if(c[i]=='9')
-		s9=s9+1;	
+		s9++;	
 	}
 	printf("%d %d %d %d %d %d %d %d %d %d",s0,s1,s2,s3,s4,s5,s6,s7,s8,s9);
 	return 0;
