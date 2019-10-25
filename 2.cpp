8355c9dcd1e3b36ec3b8c9137ac90ac99d3d796d
jatin kansal <56547404+kansaljatin@users.noreply.github.com>
2019-10-25 10:46:27 -0700

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -4,33 +4,48 @@ int main()
 	int a,b;
 	scanf("%d",&a);
 	scanf("%d",&b);
-	for(int i=a;i<=b;i++)
+	int i=a;
+	while(i<=b)
 	{
 		if(i==1)
-		printf("one\n");
+			printf("one\n");
+			printf(" ");
 		if(i==2)
-		printf("two\n");
+			printf("two\n");
+			printf(" ");
 		if(i==3)
-		printf("three\n");
+			printf("three\n");
+			printf(" ");
 		if(i==4)
-		printf("four\n");
+			printf("four\n");
 		if(i==5)
-		printf("five\n");
+			printf("five\n");
+			printf(" ");
 		if(i==6)
-		printf("six\n");
+			printf("six\n");
+			printf(" ");
 		if(i==7)
-		printf("seven\n");
+			printf("seven\n");
+			printf(" ");
 		if(i==8)
-		printf("eight\n");
+			printf("eight\n");
+			printf(" ");
 		if(i==9)
-		printf("nine\n");
+			printf("nine\n");
+			printf(" ");
+	i++;
 	}
-	for(int j=10;j<=b;j++)
+	int j=10;
+	while(j<=b)
 	{
 		if(j%2==0)
-		printf("even\n");
+			printf("even\n");
+			printf(" ");
+		
 		else
-		printf("odd\n");
+			printf("odd\n");
+			printf(" ");
+	j++;
 	}
 	return 0;
 }
